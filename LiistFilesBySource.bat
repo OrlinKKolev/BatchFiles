cb230f6a57de86aa7bd20d4c799e9a45e38f3bde
OrlinKKolev <149258809+OrlinKKolev@users.noreply.github.com>
2024-01-04 21:12:25 +0200

    Update LiistFilesBySource.bat

--- LiistFilesBySource.bat
+++ LiistFilesBySource.bat
@@ -1,3 +1,14 @@
-chdir /d <SOURCE FOLDER>"C:\Users\TAL\Desktop\temp\Athens Corian"
+chdir /d <SOURCE FOLDER>"C:\Users\Desktop\temp\Athens Corian"
 echo off
-for /r . %%g in (*) do echo %%~nxg >> <DESTINATION>"C:\Users\TAL\Desktop\temp\test.txt"
+for /r . %%g in (*) do echo %%~nxg >> <DESTINATION>"C:\Users\Desktop\temp\test.txt"
+
+::this batch file lists all files from selected <SOURCE FOLDER>
+::and writes them in .txt at the designated <DESTINATION>
+::in order to work just replace <SOURCE FOLDER> and <DESTINATION> with your desires and run the file
+
+::example for working file on my machine
+::------------------------------------------------
+::chdir /d "C:\Users\Desktop\temp\Athens Corian"
+::echo off
+::for /r . %%g in (*) do echo %%~nxg >> "C:\Users\Desktop\temp\test.txt"
+::------------------------------------------------
